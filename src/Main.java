2d94fe81a51dde8d55bacd43e32017b54654487b
Ethan <ethanxxjohnson@gmail.com>
2025-03-12 10:40:21 -0400

    task 3 final commit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         for (int i = 0; i < dataPoints.length; i++){
            dataPoints[i] = rand.nextInt(100) + 1;
         }
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
     }
 }
