7d6296e51f3b92be6fc66321f86e7631262584ec
Ethan <ethanxxjohnson@gmail.com>
2025-03-15 09:39:08 -0400

    task 5 final commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int[] dataPoints = new int[100];
         Random rand = new Random();
 
-        for (int i = 0; i < dataPoints.length; i++){
-           dataPoints[i] = rand.nextInt(100) + 1;
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
         }
 
         for (int i = 0; i < dataPoints.length; i++) {
@@ -15,5 +17,19 @@ public class Main {
                 System.out.print(" | ");
             }
         }
+        System.out.println();
+
+        int sum = 0;
+        for (int num : dataPoints) {
+            sum += num;
+        }
+
+        double average = (double) sum / dataPoints.length;
+
+        System.out.println("The sum of the values in dataPoints is: " + sum);
+        System.out.println("The average of the values in dataPoints is: " + average);
+
+        int userValue = SafeInput.getRangedInt(scanner, "Enter a number between", 1, 100);
+        System.out.println("You entered: " + userValue);
     }
 }
