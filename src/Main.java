910af5b35780a1b3da9d1928755d5e797bde5504
Ethan <ethanxxjohnson@gmail.com>
2025-03-15 09:45:43 -0400

    task 9 final commit

--- src/Main.java
+++ src/Main.java
@@ -19,58 +19,14 @@ public class Main {
         }
         System.out.println();
 
+        System.out.println("The average of dataPoints is: " + getAverage(dataPoints));
+    }
+
+    public static double getAverage(int values[]) {
         int sum = 0;
-        for (int num : dataPoints) {
+        for (int num : values) {
             sum += num;
         }
-
-        double average = (double) sum / dataPoints.length;
-
-        System.out.println("The sum of the values in dataPoints is: " + sum);
-        System.out.println("The average of the values in dataPoints is: " + average);
-
-        int userValue = SafeInput.getRangedInt(scanner, "Enter a number between", 1, 100);
-        System.out.println("You entered: " + userValue);
-
-        int count = 0;
-        for (int num : dataPoints) {
-            if (num == userValue) {
-                count++;
-            }
-        }
-        System.out.println("The value " + userValue + " was found " + count + " times in the array.");
-
-        int userValue2 = SafeInput.getRangedInt(scanner, "Enter another number between", 1, 100);
-        System.out.println("You entered: " + userValue2);
-
-        int position = -1;
-        for (int i = 0; i < dataPoints.length; i++) {
-            if (dataPoints[i] == userValue2) {
-                position = i;
-                break;
-            }
-        }
-
-        if (position != -1) {
-            System.out.println("The value " + userValue2 + " was found at array index " + position + ".");
-        } else {
-            System.out.println("The value " + userValue2 + " was not found in the array.");
-        }
-
-        // Find the minimum and maximum values
-        int min = dataPoints[0];
-        int max = dataPoints[0];
-
-        for (int i = 1; i < dataPoints.length; i++) {
-            if (dataPoints[i] < min) {
-                min = dataPoints[i];
-            }
-            if (dataPoints[i] > max) {
-                max = dataPoints[i];
-            }
-        }
-
-        System.out.println("The minimum value in the array is: " + min);
-        System.out.println("The maximum value in the array is: " + max);
+        return (double) sum / values.length;
     }
 }
