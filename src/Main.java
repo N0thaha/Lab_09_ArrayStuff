acaa4eb267a608027947dfc4e46ff2943f019675
Ethan <ethanxxjohnson@gmail.com>
2025-03-15 09:44:38 -0400

    task 8 final commit

--- src/Main.java
+++ src/Main.java
@@ -56,5 +56,21 @@ public class Main {
         } else {
             System.out.println("The value " + userValue2 + " was not found in the array.");
         }
+
+        // Find the minimum and maximum values
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+
+        System.out.println("The minimum value in the array is: " + min);
+        System.out.println("The maximum value in the array is: " + max);
     }
 }
